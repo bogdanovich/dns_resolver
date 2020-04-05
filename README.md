d38281f6337cdb88027e29431209c94dac7b3a22
Security Guy <securityguy@users.noreply.github.com>
2020-04-05 12:16:47 -0400

    Added IPv6 lookup support

--- README.md
+++ README.md
@@ -1,24 +1,25 @@
 # Simple dns resolver implemented in go
-[![Build Status](https://travis-ci.org/bogdanovich/dns_resolver.svg?branch=master)](https://travis-ci.org/bogdanovich/dns_resolver)
-[![Go Walker](http://gowalker.org/api/v1/badge)](https://gowalker.org/github.com/bogdanovich/dns_resolver)
+[![Go Walker](http://gowalker.org/api/v1/badge)](https://gowalker.org/github.com/securityguy/dns_resolver)
 
-Forked from bogdanovich/dns_resolver
+Forked from bogdanovich/dns_resolver to add IPv6 lookup
+
 Based on based on miekg/dns.
 
 ## Features
 
 - Uses provided dns servers array in random order
 - Retries dns requests in case of i/o timeout
+- IPv4 and IPv6
 
 ## Installing
 
 ### Using *go get*
 
-    $ go get github.com/bogdanovich/dns_resolver
+    $ go get github.com/securityguy/dns_resolver
 
 After this command *dns_resolver* is ready to use. Its source will be in:
 
-    $GOPATH/src/github.com/bogdanovich/dns_resolver
+    $GOPATH/src/github.com/securityguy/dns_resolver
 
 ## Example
 
@@ -27,7 +28,7 @@ package main
 
 import (
 	"log"
-	"github.com/bogdanovich/dns_resolver"
+	"github.com/securityguy/dns_resolver"
 )
 
 func main() {
@@ -43,7 +44,12 @@ func main() {
 		log.Fatal(err.Error())
 	}
 	log.Println(ip)
-	// Output [216.58.192.46]
+
+    ip, err := resolver.LookupHost6("google.com")
+    if err != nil {
+        log.Fatal(err.Error())
+    }
+    log.Println(ip)
 }
 
 ```
--- dns_resolver.go
+++ dns_resolver.go
@@ -1,5 +1,6 @@
 // Package dns_resolver is a simple dns resolver
 // based on miekg/dns
+// forked from bogdanovich/dns_resolver to add IPv6 support
 package dns_resolver
 
 import (
@@ -42,26 +43,34 @@ func NewFromResolvConf(path string) (*DnsResolver, error) {
 	return &DnsResolver{servers, len(servers) * 2, rand.New(rand.NewSource(time.Now().UnixNano()))}, err
 }
 
-// LookupHost returns IP addresses of provied host.
+// LookupHost returns IP addresses of provided host.
 // In case of timeout retries query RetryTimes times.
 func (r *DnsResolver) LookupHost(host string) ([]net.IP, error) {
-	return r.lookupHost(host, r.RetryTimes)
+	return r.lookupHost(host, r.RetryTimes, false)
 }
 
-func (r *DnsResolver) lookupHost(host string, triesLeft int) ([]net.IP, error) {
+func (r *DnsResolver) LookupHost6(host string) ([]net.IP, error) {
+	return r.lookupHost(host, r.RetryTimes, true)
+}
+
+func (r *DnsResolver) lookupHost(host string, triesLeft int, ipv6 bool) ([]net.IP, error) {
 	m1 := new(dns.Msg)
 	m1.Id = dns.Id()
 	m1.RecursionDesired = true
 	m1.Question = make([]dns.Question, 1)
-	m1.Question[0] = dns.Question{dns.Fqdn(host), dns.TypeA, dns.ClassINET}
-	in, err := dns.Exchange(m1, r.Servers[r.r.Intn(len(r.Servers))])
+	if ipv6 {
+		m1.Question[0] = dns.Question{dns.Fqdn(host), dns.TypeAAAA, dns.ClassINET}
+	} else {
+		m1.Question[0] = dns.Question{dns.Fqdn(host), dns.TypeA, dns.ClassINET}
+	}
 
+	in, err := dns.Exchange(m1, r.Servers[r.r.Intn(len(r.Servers))])
 	result := []net.IP{}
 
 	if err != nil {
 		if strings.HasSuffix(err.Error(), "i/o timeout") && triesLeft > 0 {
 			triesLeft--
-			return r.lookupHost(host, triesLeft)
+			return r.lookupHost(host, triesLeft, ipv6)
 		}
 		return result, err
 	}
@@ -71,8 +80,14 @@ func (r *DnsResolver) lookupHost(host string, triesLeft int) ([]net.IP, error) {
 	}
 
 	for _, record := range in.Answer {
-		if t, ok := record.(*dns.A); ok {
-			result = append(result, t.A)
+		if ipv6 {
+			if t, ok := record.(*dns.AAAA); ok {
+				result = append(result, t.AAAA)
+			}
+		} else {
+			if t, ok := record.(*dns.A); ok {
+				result = append(result, t.A)
+			}
 		}
 	}
 	return result, err
